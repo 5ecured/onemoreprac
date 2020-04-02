cd645ebfeae4bc6551f56af2a6ae7274b9899ae2
Edward Ottorino <edward.ottorino@gmail.com>
2020-04-02 14:28:20 +1100

    'commit'

---
+++ build/precache-manifest.1b49ba1d0613c572b8328d10791df7b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ac86f34a23233e05a68e2f001c471e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50713da33393783decb3",
+    "url": "/static/css/main.48a7c636.chunk.css"
+  },
+  {
+    "revision": "d61a71ae0ed73c850e72",
+    "url": "/static/js/2.5f3c5a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f3c5a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50713da33393783decb3",
+    "url": "/static/js/main.ccb552e9.chunk.js"
+  },
+  {
+    "revision": "712b09eabf1d604813a1",
+    "url": "/static/js/runtime-main.b8c1ee20.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const Player = require('./models/players')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+app.get('/players', (req, res) => {
+    Player.find({}).then(data => res.send(data))
+})
+
+app.post('/players', (req, res) => {
+    const body = req.body
+    const newPlayer = new Player({
+        name: body.name,
+        club: body.club,
+        important: body.important
+    })
+
+    newPlayer.save().then(data => res.send(data))
+})
+
+app.delete('/players/:id', (req, res) => {
+    Player.findByIdAndRemove(req.params.id).then(data => res.status(204).end())
+})
+
+app.put('/players/:id', (req, res) => {
+    const body = req.body
+    const newPlayer = {
+        name: body.name,
+        club: body.club,
+        important: body.important
+    }
+    Player.findByIdAndUpdate(req.params.id, newPlayer, {new: true}).then(data => res.send(data))
+})
+
+app.patch('/players/:id', (req, res) => {
+    const body = req.body
+    const newPlayer = {...body, important: !body.important}
+    Player.findByIdAndUpdate(req.params.id, newPlayer, {new: true}).then(data => res.send(data))
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
